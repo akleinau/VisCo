fc9f2ea059d96c2a44f10d0342b4fec317241d14
akleinau <56925445+akleinau@users.noreply.github.com>
2020-06-15 20:38:24 +0200

    split up in two views

--- JS/changeView.js
+++ JS/changeView.js
@@ -1,6 +1,34 @@
 function changeView() {
         Image = document.getElementById("ViewImage");
-        if (Image.src.endsWith("images/globe.png")) {Image.src = 'images/germany.png';}
-        else if (Image.src.endsWith('images/germany.png')){ Image.src = 'images/globe.png';}
+        if (Image.src.endsWith("images/globe.png")) {
+            Image.src = 'images/germany.png';
+            
+            globalObjects = document.getElementsByClassName("global");
+
+            for (var i = 0; i < globalObjects.length; i ++) {
+                globalObjects[i].style.visibility = 'visible';
+            }
+
+            germanyObjects = document.getElementsByClassName("germany");
+
+            for (var i = 0; i < germanyObjects.length; i ++) {
+                germanyObjects[i].style.visibility = 'hidden';
+            }
+
+        }
+        else if (Image.src.endsWith('images/germany.png')){ 
+            Image.src = 'images/globe.png';
+
+            globalObjects = document.getElementsByClassName("global");
+
+            for (var i = 0; i < globalObjects.length; i ++) {
+                globalObjects[i].style.visibility = 'hidden';
+            }
+
+            germanyObjects = document.getElementsByClassName("germany");
+
+            for (var i = 0; i < germanyObjects.length; i ++) {
+                germanyObjects[i].style.visibility = 'visible';
+            }}
     
 }
