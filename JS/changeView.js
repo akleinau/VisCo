eaaa2fd9749387df4150be6ae0c755ad8fa45468
akleinau <56925445+akleinau@users.noreply.github.com>
2020-06-16 14:06:08 +0200

    change headings with views

--- JS/changeView.js
+++ JS/changeView.js
@@ -1,15 +1,16 @@
 function changeView() {
     Image = document.getElementById("view-image");
-    //change to germany view
+    //change to global view
     if (Image.src.endsWith("images/globe.png")) {
         Image.src = 'images/germany.png';
-        countryName1 = document.getElementById("country1").value = "Germany";
+        document.getElementById("right-col-2").innerHTML = "compare Countries";
     }
 
-    //change to global view
+    //change to germany view
     else if (Image.src.endsWith('images/germany.png')) {
         Image.src = 'images/globe.png';
-        
+        countryName1 = document.getElementById("country1").value = "Germany";
+        document.getElementById("right-col-2").innerHTML = "Germany";
     }
 
     updateCompareGraph();
