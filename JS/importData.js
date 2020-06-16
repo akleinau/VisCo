e4c46e0ca1b2a50a8d05f91daf8358779e5f8cc3
chlang0815 <christopher.lang@ovguesports.de>
2020-06-16 14:50:14 +0200

    name changes

--- JS/importData.js
+++ JS/importData.js
@@ -1,6 +1,6 @@
 var urls = {
-    states: "data/3_mittel.geo.json",
-    counries:"",
+    states: "data/germany.geo.json",
+    countries:"data/globe.geo.json",
     coronaStates: "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Coronafälle_in_den_Bundesländern/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json",
     coronaWorldConfirmed: "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
     coronaWorldRecovered: "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv",
