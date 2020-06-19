d80b07a3d94261311257333ba46af55821e61b50
Kai Ostendorf <kaiostendorf@freenet.de>
2020-06-19 16:11:54 +0200

    table bugfixes

--- JS/changeView.js
+++ JS/changeView.js
@@ -4,6 +4,16 @@ function changeView() {
     if (Image.src.endsWith("images/globe.png")) {
         Image.src = 'images/germany.png';
         document.getElementById("right-col-2").innerHTML = "compare Countries";
+
+
+
+        document.getElementById("confirmedCases").className = "tablinks active";
+        document.getElementById("Confirmed Cases").style.display = "block";
+        document.getElementById("recovered").className = "tablinks";
+        document.getElementById("deaths").className = "tablinks";
+
+        
+       
     }
 
     //change to germany view
@@ -11,11 +21,21 @@ function changeView() {
         Image.src = 'images/globe.png';
         countryName1 = document.getElementById("country1").value = "Germany";
         document.getElementById("right-col-2").innerHTML = "Germany";
+
+        document.getElementById("death").className = "tablinks active";
+        document.getElementById("Death").style.display = "block";
+        document.getElementById("fallzahl").className = "tablinks";
+        document.getElementById("Fallzahl").style.display = "hide";
+        document.getElementById("pro100000").className = "tablinks";
+        document.getElementById("Pro100000").style.display = "hide";
+
+        
     }
 
     updateCompareGraph();
     $('.germany, .global').toggle();
-   
+    document.getElementById("data-type").value = "confirmed";
+
 }
 
 
@@ -23,3 +43,5 @@ function changeView() {
 $(function () {
     $('.global').hide();
 });
+
+
